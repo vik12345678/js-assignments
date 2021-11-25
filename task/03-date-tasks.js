58a82ba603863e368ec74d0bf8efe53f360220a7
vik12345678 <3533725@gmail.com>
2021-11-25 20:07:52 +0200

    New task

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -84,7 +84,8 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+	return (endDate - startDate) / (60 * 60 * 24 * 1000);
+   //throw new Error('Not implemented');
 }
 
 
