b4104874cfac266f354188b17a60d158a0cb6a91
vik12345678 <3533725@gmail.com>
2021-11-28 16:54:45 +0200

    New 18

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -23,6 +23,8 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
+	
+	
     throw new Error('Not implemented');
 }
 
@@ -38,7 +40,10 @@ function Rectangle(width, height) {
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+	
+	 let temp = JSON.stringify(obj);
+	 return temp;
+//    throw new Error('Not implemented');
 }
 
 
@@ -54,6 +59,13 @@ function getJSON(obj) {
  *
  */
 function fromJSON(proto, json) {
+	
+	    const obj = JSON.parse(json);
+    const values = Object.values(obj);
+    
+    return new proto.constructor(...values);
+	
+	
     throw new Error('Not implemented');
 }
 
